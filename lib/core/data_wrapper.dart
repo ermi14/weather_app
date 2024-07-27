16db098f5bda306f68ec7bbd870ae9ba3b5fdbab
ermi14 <ermi@MacBook-Pro.local>
2024-07-27 19:04:18 +0300

    removing loading from the data wrapper

--- lib/core/data_wrapper.dart
+++ lib/core/data_wrapper.dart
@@ -3,19 +3,14 @@ import 'package:coopah_frontend_dev_task/core/network_exception.dart';
 class DataWrapper<T> {
   final T? data;
   final NetworkException? error;
-  final bool loading;
 
-  DataWrapper({this.data, this.error, this.loading = false});
+  DataWrapper({this.data, this.error});
 
   factory DataWrapper.success(T data) {
-    return DataWrapper(data: data, loading: false);
+    return DataWrapper(data: data);
   }
 
   factory DataWrapper.error(NetworkException error) {
-    return DataWrapper(error: error, loading: false);
-  }
-
-  factory DataWrapper.loading() {
-    return DataWrapper(loading: true);
+    return DataWrapper(error: error);
   }
 }
